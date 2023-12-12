4bdcd819da276f55f08fcb83474047f7ca3aa21c
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-12-12 21:49:54 +0900

    private の理解

--- Level4/ContentView.swift
+++ Level4/ContentView.swift
@@ -7,15 +7,35 @@
 
 import SwiftUI
 
+class Taxi {
+    init(driver: String) {
+        self.driver = driver
+        self.driver = addDot(name: driver)
+    }
+    private var driver: String
+    
+    private func addDot(name: String) -> String {
+        return name + "."
+    }
+    
+    func getDriverName() -> String {
+        return driver
+    }
+}
+
 struct ContentView: View {
+    
+    var myTaxi = Taxi(driver: "Sasha")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("The driver is ")
+            Button {
+                print(myTaxi.getDriverName())
+            } label: {
+                Text("Change")
+            }
         }
-        .padding()
     }
 }
 
