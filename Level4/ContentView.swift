61c68a4e2c33f32cdb77da79f1a39aa4c6d94236
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-01-04 21:59:37 +0900

    generic の理解

--- Level4/ContentView.swift
+++ Level4/ContentView.swift
@@ -7,56 +7,41 @@
 
 import SwiftUI
 
-class Cloth {
-    let name: String
-    
-    init(name: String) {
-        self.name = name
-    }
-    
-    func sayName() {
-        print(name)
-    }
-}
-
-class Shirt: Cloth {
-    let size: Int
-    
-    init(size: Int, name: String) {
-        self.size = size
-        super.init(name: name)
-    }
-    
-    func saySasha() {
-        print("Sasha")
-    }
-}
-
 struct ContentView: View {
     
     var body: some View {
         VStack {
             Button {
-                let myCloth = Cloth(name: "Dress")
-                let myShirt = Shirt(size: 95, name: "Shirt")
-                
-                if myCloth is Cloth {
-                    print("This is Cloth.")
-                } else {
-                    print("This is not Cloth")
-                }
-                
-                if myCloth is Shirt {
-                    print("This is Shirt")
-                } else {
-                    print("This is not Shirt")
-                }
-                
+                let number1 = 3.4
+                let number2 = 5.4
+                genericCalculator(number1, number2)
             } label: {
-                Text("Change")
+                Text("Calculator")
             }
         }
     }
+    
+    private func genericCalculator<T> (_ first: T, _ second: T) {
+        print(first, second)
+        
+        let temp: T
+        var myFirst = first
+        var mySecond = second
+        temp = myFirst
+        myFirst = mySecond
+        mySecond = temp
+        
+        print(myFirst, mySecond)
+    }
+    
+    private func intCalculator(_ first: Int, _ second: Int) -> Int {
+        return first + second
+    }
+    
+    private func doubleCalculator(_ first: Double, _ second: Double) -> Double {
+        return first + second
+    }
+    
 }
 
 #Preview {
