5d9ddfc81448903ebf3a89a39a90d4042ff70f1c
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-01-09 21:49:43 +0900

    async image の理解

--- Level4/ContentView.swift
+++ Level4/ContentView.swift
@@ -9,21 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let imageURLString = "https://yt3.ggpht.com/pdTrKuYp6faTiDVxyNqzYDRu23o4NojHEjx5vZjBH8kYTIyRTJDrH_uTQVRLIJ-qV0Zb6DxdGQ=s900-c-k-c0x00ffffff-no-rj"
+    
     var body: some View {
-        TimelineView(.periodic(from: Date(), by: 1.0)) { context in
+        VStack {
+            Image(systemName: "bolt")
+                .data(url: URL(string: imageURLString)!)
+                .scaledToFit()
+                .frame(width: 50, height: 50)
             
-            let date = context.date
-            let seconds = Calendar.current.component(.second, from: date)
-            
-            Circle()
-                .trim(from: 0, to: Double(seconds) / 60.0)
-                .stroke(seconds > 50 ? .red : .blue, lineWidth: 30)
-                .rotationEffect(Angle(degrees: -90))
-                .overlay {
-                    Text("\(seconds)")
-                }
-                .padding()
+            AsyncImage(url: URL(string: imageURLString)) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 50, height: 50)
         }
+    }
+}
+
+extension Image {
+    func data(url: URL) -> Self {
+        if let data = try? Data(contentsOf: url) {
+            return Image(uiImage: UIImage(data: data)!)
+                .resizable()
+        }
+        return self
+            .resizable()
     }
 }
 
