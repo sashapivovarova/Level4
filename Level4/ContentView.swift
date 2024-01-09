0da420e65bfe744a35796bf92e6d732f1b109aba
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-01-09 21:21:52 +0900

    timeline view の理解1

--- Level4/ContentView.swift
+++ Level4/ContentView.swift
@@ -8,40 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    struct FileItem: Hashable, Identifiable, CustomStringConvertible {
-        var id: Self { self }
-        var name: String
-        var children: [FileItem]? = nil
-        var description: String {
-            switch children {
-            case nil:
-                return "📄 \(name)"
-            case .some(let children):
-                return children.isEmpty ? "📂 \(name)" : "📁 \(name)"
-            }
-        }
-    }
-
-    let data =
-      FileItem(name: "users", children:
-        [FileItem(name: "user1234", children:
-          [FileItem(name: "Photos", children:
-            [FileItem(name: "photo001.jpg"),
-             FileItem(name: "photo002.jpg")]),
-           FileItem(name: "Movies", children:
-             [FileItem(name: "movie001.mp4")]),
-              FileItem(name: "Documents", children: [])
-          ]),
-         FileItem(name: "newuser", children:
-           [FileItem(name: "Documents", children: [])
-           ])
-        ])
     
     var body: some View {
-        OutlineGroup(data, children: \.children) { item in
-            Text("\(item.description)")
+        TimelineView(.periodic(from: Date(), by: 1.0)) { context in
+            Text("\(context.date)")
         }
-        .padding()
     }
 }
 
