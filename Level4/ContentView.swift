92ab0da58343fefef8e76c50e73c2e7388eb216c
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-01-11 21:13:39 +0900

    group の理解

--- Level4/ContentView.swift
+++ Level4/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var myURL = URL(string: "https://developer.apple.com/xcode/swiftui/")
-    
     var body: some View {
         VStack {
-            ShareLink(item: myURL!) {
-                Label("Share", image: "MyCustomShareIcon")
-            }
+            Group {
+                        Text("1")
+                        Text("2")
+                        Text("3")
+                        Text("4")
+                        Text("5")
+                        Text("6")
+                        Text("7")
+                        Text("8")
+                        Text("9")
+                        Text("10")
+                    }
+                    Text("11")
         }
     }
 }
