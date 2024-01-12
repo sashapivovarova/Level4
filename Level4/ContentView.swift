e662b880df1ea984216a773251bed714d06f7950
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-01-12 22:06:23 +0900

    group box の理解

--- Level4/ContentView.swift
+++ Level4/ContentView.swift
@@ -9,21 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var agreementText = "In order to use Apple’s developer tools and services, or distribute software on Apple platforms, you’ll need to adhere to the applicable terms in the following agreements and guidelines. Be certain to read the relevant terms carefully as you design and develop your software. Please note that the English language version of the agreements you accept in your developer account are binding and the most up to date. Any other language translations of select agreements that we may provide are for your convenience."
+    @State var userAgreed = false
+    
     var body: some View {
         VStack {
-            Group {
-                        Text("1")
-                        Text("2")
-                        Text("3")
-                        Text("4")
-                        Text("5")
-                        Text("6")
-                        Text("7")
-                        Text("8")
-                        Text("9")
-                        Text("10")
-                    }
-                    Text("11")
+            GroupBox(label:
+                        Label("End-User Agreement", systemImage: "building.columns")
+            ) {
+                ScrollView(.vertical, showsIndicators: true) {
+                    Text(agreementText)
+                        .font(.footnote)
+                }
+                .frame(height: 100)
+                Toggle(isOn: $userAgreed) {
+                    Text("I agree to the above terms")
+                }
+            }
+            GroupBox {
+                ScrollView(.vertical, showsIndicators: true) {
+                    Text(agreementText)
+                        .font(.footnote)
+                }
+                .frame(height: 100)
+                Toggle(isOn: $userAgreed) {
+                    Text("I agree to the above terms")
+                }
+            } label: {
+                Label("Security", systemImage: "lock")
+            }
         }
     }
 }
